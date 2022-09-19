9ce2f0f18f70a62bb61215ce0e3d85141e0836fe
Harsholly <shola.abigail1@gmail.com>
2022-09-19 13:02:51 -0700

    main.h

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
-/**
+**
  *  * reset_to_98 - changes the value of the variable
- *   * that's pointed to by the argument to 98
- *    *@n: pointer variable
+*    *@n: pointer variable
  *     */
 void reset_to_98(int *n)
 {
