484cca525b91764f96242bbaab2fca5632a40717
Harsholly <shola.abigail1@gmail.com>
2022-09-22 10:24:01 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * infinite_add - adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: buffer for result
- * @size_r: buffer size
- *
- * Return: address of r or 0
+ * infinite_add - adds two numbers.
+ * @n1: first number.
+ * @n2: second number.
+ * @r: result.
+ * @size_r: result size.
+ * Return: the addition of n1 and n2.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
