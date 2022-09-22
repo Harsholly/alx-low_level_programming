397995e87335689fbbb299bf1663c4bc9fea94d8
Harsholly <shola.abigail1@gmail.com>
2022-09-22 09:47:50 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -23,7 +23,7 @@ void print_buffer(char *b, int size)
 				if (k % 2 == 0)
 					printf(" ");
 				if (k < size)
-					printf("%.2x", *(b + k)); 
+					printf("%.2x", *(b + k));
 				else
 					printf("  ");
 			}
@@ -34,7 +34,7 @@ void print_buffer(char *b, int size)
 					break;
 				if (*(b + l) < 32 || *(b + l) > 126)
 					printf("%c", '.');
-				else 
+				else
 					printf("%c", *(b + l));
 			}
 			printf("\n");
